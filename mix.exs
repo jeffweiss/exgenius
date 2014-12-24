265400ed3f448ff9fbbb2069fc96f8e98c087f67
jeffweiss <jeff.weiss@puppetlabs.com>
2014-12-24 00:31:10 -0800

    bump to 0.0.4

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Exgenius.Mixfile do
 
   def project do
     [app: :exgenius,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.0",
      name: "ExGenius",
      description: @description,
